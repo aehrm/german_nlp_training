d95fd3a5533cd164b69c1a72e2524487222e96ac
Anton Ehrmanntraut <anton.ehrmanntraut@uni-wuerzburg.de>
2023-01-20 18:41:42 +0100

    wip: Adjust code for usage in Kubernetes clusters

--- uhh-lt_neural-coref/train_droc_model_from_scratch.sh
+++ uhh-lt_neural-coref/train_droc_model_from_scratch.sh
@@ -9,9 +9,12 @@ MODEL_TYPE=${MODEL_TYPE:-bert}
 python3 preprocess.py --input_dir data/ --output_dir data/tuba10 --seg_len ${SEGMENT_LENGTH} \
   --language german --tokenizer_name ${MODEL} --input_suffix tuba10_gold_conll --input_format conll-2012 \
   --model_type ${MODEL_TYPE}
-#python3 preprocess.py --input_dir data/droc_full --output_dir data/droc_full --seg_len ${SEGMENT_LENGTH} \
-#  --language german --tokenizer_name ${MODEL} --input_suffix droc_gold_conll --input_format conll-2012 \
-#  --model_type ${MODEL_TYPE}
+python3 preprocess.py --input_dir data/droc_full --output_dir data/droc_full --seg_len ${SEGMENT_LENGTH} \
+  --language german --tokenizer_name ${MODEL} --input_suffix droc_gold_conll --input_format conll-2012 \
+  --model_type ${MODEL_TYPE}
+
+TIMESTAMP=$(date +%Y-%m-%dT%H:%M:%S)
+mkdir /output/$TIMESTAMP
 
 
 # append to experiments.conf
@@ -22,9 +25,16 @@ tuba10_custom = \${tuba10}{
   data_dir = \${base.data_dir}/tuba10
   bert_tokenizer_name = ${MODEL}
   bert_pretrained_name_or_path = ${MODEL}
+  max_segment_len = ${SEGMENT_LENGTH}
   long_doc_strategy = split
-  log_root = ./output/
-  num_epochs = 1
+  log_root = /output/$TIMESTAMP/
+}
+
+droc_c2f_custom = \${droc_c2f}{
+  bert_tokenizer_name = ${MODEL}
+  bert_pretrained_name_or_path = ${MODEL}
+  max_segment_len = ${SEGMENT_LENGTH}
+  log_root = /output/$TIMESTAMP/
 }
 EOT
 
@@ -34,8 +44,8 @@ cp data/reference-coreference-scorers data/tuba10 -r
 echo "performing fine-tuning on the TüBa-D/Z news dataset using ${MODEL}"
 python3 run.py tuba10_custom 0
 
-#FINE_TUNED_MODEL=./output/tuba10/
-#
-## fine-tune on DROC, continuing on previous state
-#echo "performing c2f fine-tuning on the DROC dataset using ${FINE_TUNED_MODEL}"
-#python3 run.py droc_c2f 0 --model
+FINE_TUNED_MODEL=$(ls /output/$TIMESTAMP/tuba10_custom/model_*.bin)
+
+# fine-tune on DROC, continuing on previous state
+echo "performing c2f fine-tuning on the DROC dataset using ${FINE_TUNED_MODEL}"
+python3 run.py droc_c2f_custom 0 --model $FINE_TUNED_MODEL
