d07f076a5d8688c9cf7fe9a1523cd6f76227a1eb
Anton Ehrmanntraut <anton.ehrmanntraut@uni-wuerzburg.de>
2023-05-02 15:29:50 +0200

    fixup! wip: redewiedergabe tagger

--- redewiedergabe/train_model.py
+++ redewiedergabe/train_model.py
@@ -2,6 +2,7 @@ from tagger.rwtagger.tagger import RWTagger
 import pandas
 import logging
 import sys
+import os
 
 for kind in ['direct', 'indirect', 'freeIndirect', 'reported']:
     print('training ', kind)
@@ -9,6 +10,6 @@ for kind in ['direct', 'indirect', 'freeIndirect', 'reported']:
     val_df = pandas.read_csv(f'./data_konvens-paper-2020/val/{kind}_combined.tsv', sep='\t')
     test_df = pandas.read_csv(f'./data_konvens-paper-2020/test/{kind}_combined.tsv', sep='\t')
 
-    os.path.mkdir(f'./train_output/{kind}/')
+    os.mkdir(f'/output/{kind}/')
     tagger = RWTagger(device='cuda:0')
-    tagger.train(train_df, val_df, test_df, f'./train_output/{kind}/', embtype=sys.argv[1])
+    tagger.train(train_df, val_df, test_df, f'/output/{kind}', embtype=sys.argv[1])
